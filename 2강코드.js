b84412d7a85a0811bb95249c22cb437ea7034712
kds2783 <vipvip701@naver.com>
2020-10-17 05:37:01 +0900

    bot upload

---
+++ 2강코드.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NzY2NzM2MDE5NzEzNDI1NDI5.X4nskA.X0oBGGBsc9HCxwm5imkd6vCVmP4';
+const welcomeChannelName = "〔📛〕ㅣ입장로그";
+const byeChannelName = "〔📛〕ㅣ입장로그";
+const welcomeChannelComment = "``` 님께서 입장하셨습니다. ```";
+const byeChannelComment = "``` 님께서 퇴장하셨습니다. ```";
+
+client.on('ready', () => {
+  console.log('[ 어몽어스 ] 실행되었습니다.');
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
+client.on('message', (message) => {
+  if(message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
